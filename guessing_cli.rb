cb158186a9b94d47e7587cd289c613d9e14b88f5
Adnan Azim <saazim1123@gmail.com>
2019-03-24 13:30:20 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,25 @@
 # Code your solution here!
+
+def run_guessing_game
+  random_number = 1
+  
+  puts "Please enter a number or type exit to quit the game:"
+  user = gets.chomp
+  
+  until user == random_number
+    if user.to_i == random_number
+      puts "You guessed the correct number!"
+      
+    else 
+      puts "The computer guessed #{random_number}"
+    end  
+  end
+    
+    if user.downcase == "exit"
+      puts "Goodbye! "
+    end
+ 
+end
+
+
+
